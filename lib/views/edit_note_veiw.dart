2d00493ae63ac88ed444c0699ecdf6c4d8ea4112
boody <boodayman936@gmail.com>
2023-12-08 22:49:03 +0200

    edit note view ui done

--- lib/views/edit_note_veiw.dart
+++ lib/views/edit_note_veiw.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/custom_text_field.dart';
+
 class EditNoteView extends StatelessWidget {
   static String routeName = 'editView';
 
@@ -7,6 +9,35 @@ class EditNoteView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Edit Note',
+            style: TextStyle(fontSize: 24),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.check,
+                size: 24,
+              ),
+              style: IconButton.styleFrom(
+                backgroundColor: Colors.white.withOpacity(.05),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusDirectional.circular(12)),
+              ),
+            ),
+          ],
+        ),
+        body: const Padding(
+          padding: EdgeInsets.symmetric(vertical: 50, horizontal: 8),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+            CustomTextField(label: 'title'),
+            SizedBox(height: 16),
+            CustomTextField(label: 'content', maxLines: 5),
+          ]),
+        ));
   }
 }
--- lib/widgets/custom_note.dart
+++ lib/widgets/custom_note.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes/views/edit_note_veiw.dart';
 
 class CustomNote extends StatelessWidget {
   const CustomNote({
@@ -7,43 +8,48 @@ class CustomNote extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: Container(
-        padding: const EdgeInsets.symmetric(vertical: 24),
-        decoration: const BoxDecoration(
-            color: Colors.yellow,
-            borderRadius: BorderRadius.all(Radius.circular(12))),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            const ListTile(
-              title: Padding(
-                padding: EdgeInsets.only(bottom: 14.0),
-                child: Text(
-                  'flutter Notes',
-                  style: TextStyle(color: Colors.black, fontSize: 25),
+    return InkWell(
+      onTap: () {
+        Navigator.pushNamed(context, EditNoteView.routeName);
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 24),
+          decoration: const BoxDecoration(
+              color: Colors.yellow,
+              borderRadius: BorderRadius.all(Radius.circular(12))),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              const ListTile(
+                title: Padding(
+                  padding: EdgeInsets.only(bottom: 14.0),
+                  child: Text(
+                    'flutter Notes',
+                    style: TextStyle(color: Colors.black, fontSize: 25),
+                  ),
+                ),
+                subtitle: Text(
+                  'abdelrahman first Notes on this app',
+                  style: TextStyle(color: Colors.grey, fontSize: 20),
+                ),
+                trailing: Icon(
+                  Icons.delete,
+                  color: Colors.black,
+                  size: 30,
                 ),
               ),
-              subtitle: Text(
-                'abdelrahman first Notes on this app',
-                style: TextStyle(color: Colors.grey, fontSize: 20),
-              ),
-              trailing: Icon(
-                Icons.delete,
-                color: Colors.black,
-                size: 30,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 24),
-              child: Text(
-                '12/10/2023',
-                style: TextStyle(color: Colors.black.withOpacity(.5)),
-              ),
-            )
-          ],
+              Padding(
+                padding: const EdgeInsets.only(right: 24),
+                child: Text(
+                  '12/10/2023',
+                  style: TextStyle(color: Colors.black.withOpacity(.5)),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
